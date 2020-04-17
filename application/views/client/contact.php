eefa6772c664775e4e3ee3336d8bcc7ecd027ddb
huytq27 <57428965+huytq27@users.noreply.github.com>
2020-04-17 18:54:34 +0700

    udate-17042020

--- application/views/client/contact.php
+++ application/views/client/contact.php
@@ -18,7 +18,262 @@
     Trang liên hệ
     
 </div>
-    
+<div class="cb-section">
+        <div class="container py-5">
+            <!-- Vùng nguyên liệu -->
+            <div class="row py-5">
+                <div class="col-md-4">
+                    <div class="row title-section  pt-5">
+                        <div class="col-md-10">
+                            <h2 class="text-title">Farm – Vùng nguyên liệu</h2>
+                            <p><span>&#10077</span> slogan <span>&#10078</span></p>
+                        </div>
+                    </div>
+                    <div class="noiDungQuaTrinhPhatTrien">
+                        <div class="line2">
+
+                        </div>
+
+                        <p class="p1">
+                            Nông trại cà phê của CCR nằm tọa lạc tại vùng cao nguyên Buôn Ma Thuột - Đăk Lăk,
+                            Đà Lạt – Lâm đồng, Đăk Nông, Gia Lai với độ cao 600-2000m so với mực nước biển,
+                            khí hậu quanh năm đều mát mẻ, thổ nhưỡng thích hợp và có điều kiện tốt nhất cho các giống cà phê Việt Nam phát triển tối ưu.
+                        </p>
+                        <button type="button" class="btn btn-outline-info my-3 ml-3">Xem chi tiết</button>
+                    </div>
+
+                </div>
+                <div class="col-md-8">
+                    <div id="slideFarm" class="carousel slide slideQuaTrinhPhatTrien" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-interval="10000">
+                                <img src="assets/img/anh bia/b1.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item" data-interval="2000">
+                                <img src="assets/img/anh bia/b2.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="assets/img/anh bia/b3.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#slideFarm" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#slideFarm" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /Vùng nguyên liệu -->
+            <!-- Sơ chế processing -->
+            <div class="row py-5">
+                <div class="col-md-8">
+                    <div id="slideProcessing" class="carousel slide slideQuaTrinhPhatTrien" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-interval="10000">
+                                <img src="assets/img/anh bia/b3.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item" data-interval="2000">
+                                <img src="assets/img/anh bia/b2.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="assets/img/anh bia/b1.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#slideProcessing" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#slideProcessing" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="row title-section  pt-5">
+                        <div class="col-md-10">
+                            <h2 class="text-title">Sơ chế - Processing</h2>
+                            <p><span>&#10077</span> slogan <span>&#10078</span></p>
+                        </div>
+                    </div>
+                    <div class="noiDungQuaTrinhPhatTrien">
+                        <div class="line2">
+
+                        </div>
+
+                        <p>
+                            <Strong>&#10004 Rửa sạch, vớt nổi</Strong>
+                            <br class="px-2"> Loại bỏ trái hư, sâu, khô, tạp chất, đất, cành, lá,
+                            <strong><br>&#10004 Lựa bỏ trái xanh</strong>
+                            <br class="px-2"> Loại bỏ những trái xanh chưa chín
+                            <strong><br>&#10004 Phơi nhà kính, giàn phơi</strong>
+                            <br class="px-2"> Phơi nhà kính kiểm soát nhiệt độ, độ ẩm theo dõi quá trình lên men tạo ra thành phẩm tốt nhất, tránh để tiếp xúc với đất
+
+                        </p>
+                        <button type="button" class="btn btn-outline-info mt-3 ml-3">Xem chi tiết</button>
+                    </div>
+                </div>
+            </div>
+            <!-- / Sơ chế processing -->
+            <!-- xử lý lỗi -->
+            <div class="row py-5 ">
+                <div class="col-md-4 ">
+                    <div class="row title-section  pt-5">
+                        <div class="col-md-10">
+                            <h2 class="text-title">Bảo quản – lưu trữ</h2>
+                            <p><span>&#10077</span> slogan <span>&#10078</span></p>
+                        </div>
+                    </div>
+                    <div class="noiDungQuaTrinhPhatTrien">
+                        <div class="line2">
+                        </div>
+
+                        <p class="p1">
+                            Cà phê nhân sau khi được sơ chế và phơi đạt được
+                            chất lượng cũng như phân sàng và phân loại bỏ các
+                            hạt defect rồi mới được đóng vào bao lưu trữ. Kho
+                            bảo quản của chúng tôi được thiết kế và được kiểm
+                            định đủ điều kiện an toàn vệ sinh thực phẩm để chất
+                            lượng hạt cà phê luôn được bảo quản tốt nhất.
+                        </p>
+                        <button type="button" class="btn btn-outline-info mt-3 ml-3">Xem chi tiết</button>
+                    </div>
+
+                </div>
+                <div class="col-md-8">
+                    <div id="slideXuLyLoi" class="carousel slide slideQuaTrinhPhatTrien" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-interval="10000">
+                                <img src="assets/img/anh bia/b1.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item" data-interval="2000">
+                                <img src="assets/img/anh bia/b2.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="assets/img/anh bia/b3.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#slideXuLyLoi" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#slideXuLyLoi" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!--/ xử lý lỗi -->
+            <!-- Rang mộc -->
+            <div class="row py-5">
+                <div class="col-md-8">
+                    <div id="slideRoasting" class="carousel slide slideQuaTrinhPhatTrien" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-interval="10000">
+                                <img src="assets/img/anh bia/b1.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item" data-interval="2000">
+                                <img src="assets/img/anh bia/b2.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="assets/img/anh bia/b3.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#slideRoasting" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#slideRoasting" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="row title-section  pt-5">
+                        <div class="col-md-10">
+                            <h2 class="text-title">Roasting- rang mộc</h2>
+                            <p><span>&#10077</span> slogan <span>&#10078</span></p>
+                        </div>
+                    </div>
+                    <div class="noiDungQuaTrinhPhatTrien">
+                        <div class="line2">
+
+                        </div>
+
+                        <p class="p1">
+                            Cốt lõi trong mỗi quy trình của CCR là kỹ thuật rang.
+                            Đến hàng trăm nghìn phản ứng hóa học trên mỗi giây sẽ liên
+                            tục diễn ra trong lò rang. để cho ra một mẻ rang hoàn hảo đòi
+                            hỏi phải tinh vi trong kỹ thuật, điêu luyện trong làm chủ độ
+                            lửa & sành điệu trong thử nếm để phát triển hượng vị trọn vẹn
+                            trong mỗi mẻ rang. Vì thế bằng niềm đam mê,sự chuyên nghiệp và
+                            chuyên môn cao chúng tôi luôn cho ra những mẻ rang mang những
+                            hương vị tuyệt vời nhất .
+                        </p>
+                        <button type="button" class="btn btn-outline-info mt-3 ml-3">Xem chi tiết</button>
+                    </div>
+                </div>
+            </div>
+            <!--/ Rang mộc -->
+            <!-- cupping -->
+            <div class="row py-5">
+                <div class="col-md-4">
+                    <div class="row title-section  pt-5">
+                        <div class="col-md-10">
+                            <h2 class="text-title">Cupping- thử nếm/kiểm định</h2>
+                            <p><span>&#10077</span> slogan <span>&#10078</span></p>
+                        </div>
+                    </div>
+                    <div class="noiDungQuaTrinhPhatTrien">
+                        <div class="line2">
+
+                        </div>
+
+                        <p class="p1">
+                            Đây là quá trình góp một phần quan trọng trong từng
+                            sản phẩm mà xưởng sản xuất tạo ra, nó giúp ta kiểm tra –
+                            tìm hiểu – kiểm định hương vị, chất lượng, sự đồng đều
+                            và quá trình hình thành và bản chất sản phẩm như thế nào.
+                            Để “ COBA COFFEE ROASTERY ” tạo ra giá trị cốt lõi mang
+                            tới cho khách hàng sản phẩm chất lượng tốt nhất .Chúng tôi
+                            tuyệt đối chỉ gửi đến bạn những hạt cà phê thơm ngon và tươi
+                            mới mỗi ngày.
+                        </p>
+                        <button type="button" class="btn btn-outline-info mt-3 ml-3">Xem chi tiết</button>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div id="slideCupping" class="carousel slide slideQuaTrinhPhatTrien" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-interval="10000">
+                                <div class="cobaSliderTop" style="background-image: url('assets/img/San pham/bg_1.jpg')"></div>
+                            </div>
+                            <div class="carousel-item" data-interval="2000">
+                                <div class="cobaSliderTop" style="background-image: url('assets/img/San pham/bg_1.jpg')"></div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="cobaSliderTop" style="background-image: url('assets/img/San pham/bg_1.jpg')"></div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#slideCupping" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#slideCupping" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- / cupping -->
+        </div>
         
         
 
