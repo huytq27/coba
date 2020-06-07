f9987f2bb64383b78718bb9de879fc5c58a70406
huytq27 <57428965+huytq27@users.noreply.github.com>
2020-06-07 14:30:35 +0700

    huy moi

--- application/views/client/index.php
+++ application/views/client/index.php
@@ -4,7 +4,7 @@
 <head>
     <title>Coba coffee & roastery</title>
     <meta charset="utf-8">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->load->view('client/includes/css') ?>
 
 </head>
