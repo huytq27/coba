1bba55e6ce39d61522aae4046ecf9082d8667c6e
huytq27 <57428965+huytq27@users.noreply.github.com>
2020-06-08 11:18:01 +0700

    seo

--- application/views/client/includes/css.php
+++ application/views/client/includes/css.php
@@ -1,8 +1,27 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-168810108-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-168810108-1');
+</script>
+
+
+<!-- Meta -->
+<meta name="DC.title" content="Cobacoffeeroastery, ca phe buon ma thuot, cà phê bmt,cà phê đắk lắk, Cà phê co ba, ca phe coba ,ca phe dak lak" />
+<meta name="geo.region" content="VN-33" />
+<meta name="geo.placename" content="Buôn Ma Thuột" />
+<meta name="geo.position" content="12.682939;108.11043" />
+<meta name="ICBM" content="12.682939, 108.11043" />
+
+<!-- meta -->
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 <!-- end Boosstraps  -->
-
-
-
 <!-- font awesome -->
 <script src="https://kit.fontawesome.com/c409280110.js" crossorigin="anonymous"></script>
 <!-- end font awesome -->
@@ -21,4 +40,3 @@
 <link rel="icon" href="assets/img/icon/logotitle.png" />
 
 <link rel="stylesheet" href="assets/css/style.css">
-
--- application/views/client/includes/footer.php
+++ application/views/client/includes/footer.php
@@ -1,10 +1,10 @@
 <div class="container-fluid coba-footer">
     <div class="container pt-5">
-        <div class="row" >
-            <div class="col-md-3 " >
+        <div class="row">
+            <div class="col-md-3 ">
                 <img src="assets/img/logo.png" class="logo-coba-footer" alt="" data-aos="fade-in" data-aos-delay="200" data-aos-anchor-placement="center-bottom">
                 <p><Strong>Địa chỉ:</Strong> 88 Má Hai, P.Tân Hoà, TP.Buôn Ma Thuột, Tỉnh ĐăkLăk</p>
-                <p><Strong>Hotline:</Strong> 0949872587</p>
+                <p><Strong>Hotline:<a href="tel:0949872587" style="outline: none; "></Strong> 0949.872.587</p></a>
             </div>
             <div class="col-md-9">
                 <div class="row">
@@ -43,13 +43,13 @@
                     <div class="col-4 mxh-coba">
                         <h3>Liên hệ</h3>
                         <div class="w-100 d-inline" data-aos-delay="100" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-                            <a target="blank" href="https://www.facebook.com/Coba.Coffee.Roastery.2016" >
+                            <a target="blank" href="https://www.facebook.com/Coba.Coffee.Roastery.2016">
                                 <span class="fab fa-facebook-f"></span></a>
 
-                            <a href="https://www.instagram.com/coba.coffee.roastery" target="blank" >
+                            <a href="https://www.instagram.com/coba.coffee.roastery" target="blank">
                                 <span class=" fab fa-instagram"></span></a>
 
-                            <a href="#" > <span class=" far fa-envelope"></span></a>
+                            <a href="#"> <span class=" far fa-envelope"></span></a>
                         </div>
                     </div>
                 </div>
@@ -58,7 +58,7 @@
     </div>
     <div class="row footer-bottom">
         <div class="container text-center">
-            <p class="my-2">Copyright <i class="far fa-copyright"></i> 2020 By QUANG HUY</p>
+            <p class="my-2">Copyright <i class="far fa-copyright"></i> 2020 By <a href="https://www.facebook.com/Quang.Huy.97.Ta" target="blank">TQHTeam</a></p>
         </div>
 
 
